432b770c32c9dd6b5e3e812ad901aa14c6cd69d4
Niek Brouwer <N.Brouwer-1@student.tudelft.nl>
2020-01-25 21:20:34 +0100

    works till login

--- first_bot.py
+++ first_bot.py
@@ -1,6 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 class ticket_buyer:
 
@@ -15,12 +14,16 @@ class ticket_buyer:
         search_box = self.driver.find_element_by_xpath("//input[@placeholder=\"Zoeken naar evenementen, locaties en steden\"]")
         time.sleep(1)
         search_box.send_keys(self.festival)
-        time.sleep(5)
+        time.sleep(3)
         festival_box = self.driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/h4').click()
-        time.sleep(5)
+        time.sleep(1)
         ticket_box = self.driver.find_element_by_xpath(f'//h4[text()="{self.ticket}"]')
         self.driver.execute_script("arguments[0].click();", ticket_box)
+        time.sleep(5)
         print('ticket found')
+        self.driver.find_element_by_xpath('/html/body/div[3]/div/div/button').click()
+        time.sleep(1)
+
 
     def find_available(self):
         result = True
@@ -35,26 +38,13 @@ class ticket_buyer:
         print('...finding ticket')
 
     def buyer(self):
-        # self.driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[3]/div/div[1]/ul/div[1]/a/div/header/h3').click()
-        self.driver.find_element_by_xpath('//button[text()="Koop een ticket"]').click()
+        available_ticket = self.driver.find_element_by_xpath(f'//h3[contains(text(),"{self.ticket}")]')
+        self.driver.execute_script("arguments[0].click();", available_ticket)
+        time.sleep(1)
+        clicker = self.driver.find_element_by_xpath('//button[text()="Koop een ticket"]')
+        self.driver.execute_script("arguments[0].click();", clicker)
+        time.sleep(5)
+        return True
 
 
-def main():
-    festival = input('Which festival would you like to go?\n')
-    ticket = input('Which kind of ticket would you like to buy?\n')
-
-    customer_choice = ticket_buyer(festival, ticket)
-
-    customer_choice.ticket_finder()
-    available = customer_choice.find_available()
-
-    if available is True:
-         customer_choice.buyer()
-    if available is False:
-        while customer_choice.find_available() is False:
-            customer_choice.refresher()
-
-        if customer_choice.find_available() is True:
-            customer_choice.buyer()
-
-main()
+# customer_choice = ticket_buyer('Dekmantel festival 2020', '4 Dagenkaart')
