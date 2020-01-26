6403ddf495518bece9abed101e271d155cb9631f
Niek Brouwer <N.Brouwer-1@student.tudelft.nl>
2020-01-26 15:08:52 +0100

    working on accessing fb account

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from password_encrypter import classified_information
 from first_bot import ticket_buyer
-from automated_sign_in import sign_in
 
 
-def main():
+def main_buyer():
     festival = input('Which festival would you like to go?\n')
     ticket = input('Which kind of ticket would you like to buy?\n')
 
@@ -17,11 +16,16 @@ def main():
 
     if available is True:
         customer_choice.buyer()
+        decrypt = classified_information('', 1, 'pw_file.txt')
         f = open('pw_file.txt', 'r')
         f1 = f.readlines()
         encrypted_email = f1[0]
         encrypted_password = f1[1]
-        email = classified_information.decrypter(encrypted_email)
-        password = classified_information.decrypter(encrypted_password)
-        sign_in(email, password)
+        email = str(decrypt.decrypter(encrypted_email))
+        password = str(decrypt.decrypter(encrypted_password))
+        customer_choice.sign_in(email, password)
 
+if __name__ == '__main__':
+    decrypt = classified_information('', 1, 'pw_file.txt')
+    decrypt.main_encrypter()
+    main_buyer()
